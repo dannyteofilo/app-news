cc6a63940e18a01f70ad45d3a6fe552529afe06d
dannyteofilo <danyel.285309@gmail.com>
2019-04-07 01:00:09 -0600

    structure of the project created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+  declarations: [HeaderComponent, SidebarComponent, NopagefoundComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    NopagefoundComponent
+  ]
+})
+export class SharedModule { }
